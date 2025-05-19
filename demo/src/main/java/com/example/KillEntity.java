c818674222fade2511f25e2ea2796f26c9094b34
Daniel E. Nissen <dniss24@student.sdu.dk>
2025-05-19 15:22:55 +0200

    Updated killentity

--- demo/src/main/java/com/example/KillEntity.java
+++ demo/src/main/java/com/example/KillEntity.java
@@ -1,7 +1,7 @@
 package com.example;
 
 import com.example.Ghost.states;
-// import com.example.MoveableBlock.direction;
+import com.example.MoveableBlock.direction;
 
 public class KillEntity implements IKillEntity {
 
@@ -29,11 +29,16 @@ public class KillEntity implements IKillEntity {
     public void killPlayer() {
         lives.removeLife();
         
-        // pacman.setDirection(direction.NONE);
-        // redGhost.setDirection(direction.NONE);
-        // blueGhost.setDirection(direction.NONE);
-        // pinkGhost.setDirection(direction.NONE);
-        // orangeGhost.setDirection(direction.NONE);
+        pacman.setDirection(direction.NONE);
+        pacman.setBufferDirection(direction.NONE);
+        redGhost.setDirection(direction.NONE);
+        redGhost.setBufferDirection(direction.NONE);
+        blueGhost.setDirection(direction.NONE);
+        blueGhost.setBufferDirection(direction.NONE);
+        pinkGhost.setDirection(direction.NONE);
+        pinkGhost.setBufferDirection(direction.NONE);
+        orangeGhost.setDirection(direction.NONE);
+        orangeGhost.setBufferDirection(direction.NONE);
 
         pacman.setPos(pacman.getStartX(), pacman.getStartY());
         redGhost.setPos(redGhost.getStartX(), redGhost.getStartY());
