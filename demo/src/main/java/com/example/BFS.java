a08ec227c58599be11396bd96a42e2dedae65c35
sihol24 <sihol24@student.sdu.dk>
2025-05-23 12:42:32 +0200

    git hard

--- demo/src/main/java/com/example/BFS.java
+++ demo/src/main/java/com/example/BFS.java
@@ -17,86 +17,93 @@ public class BFS {
     }
 
     public direction search(Ghost ghost) {
-        System.out.println("Searching for pacman");
         Node startNode = grid.getNode(grid.toCol(ghost.getX()), grid.toRow(ghost.getY()));
         Node targetNode = grid.getNode(grid.toCol(map.getPacman().getX()), grid.toRow(map.getPacman().getY()));
-        ArrayList<Node> possibleNodes = new ArrayList<>();
+        
+        // Reset all nodes for a clean search
+        for (Node node : grid.getAllNodes()) {
+            node.setSeen(false);
+            node.setNeighborsNodeDirection(null);
+        }
+        
+        ArrayList<Node> queue = new ArrayList<>();
         ArrayList<Node> seenNodes = new ArrayList<>();
         boolean found = false;
-
-
-        Node[] startneighbors = startNode.getNeighbourgs(); 
-        System.out.println("Start node: " + startneighbors.length);
+        
+        // Mark start node as seen
+        startNode.setSeen(true);
+        seenNodes.add(startNode);
+        
+        // Add initial neighbors with directions
+        Node[] startneighbors = startNode.getNeighbourgs();
         for (int i = 0; i < startneighbors.length; i++) {
-            if (startneighbors[i] != null && startneighbors[i] != targetNode) {
-                possibleNodes.add(startneighbors[i]);
-               
-                //System.out.println(startneighbors[i]);
-                //System.out.println(possibleNodes);
+            if (startneighbors[i] != null) {
+                // Check if this neighbor is already Pacman
+                if (startneighbors[i] == targetNode) {
+                    // Found Pacman immediately!
+                    switch (i) {
+                        case 0: return direction.UP;
+                        case 1: return direction.DOWN;
+                        case 2: return direction.LEFT;
+                        case 3: return direction.RIGHT;
+                        default: return direction.NONE;
+                    }
+                }
+                
+                queue.add(startneighbors[i]);
+                startneighbors[i].setSeen(true);
+                seenNodes.add(startneighbors[i]);
+                
+                // Set direction based on index
                 switch (i) {
-                    case 0:
-                        startneighbors[i].setNeighborsNodeDirection(neighborDirection.UP);
-                        break;
-                    case 1:
-                        startneighbors[i].setNeighborsNodeDirection(neighborDirection.DOWN);
-                        break;
-                    case 2:
-                        startneighbors[i].setNeighborsNodeDirection(neighborDirection.LEFT);
-                        break;
-                    case 3:
-                        startneighbors[i].setNeighborsNodeDirection(neighborDirection.RIGHT);
-                        break;
-                    default:
-                        break;
+                    case 0: startneighbors[i].setNeighborsNodeDirection(neighborDirection.UP); break;
+                    case 1: startneighbors[i].setNeighborsNodeDirection(neighborDirection.DOWN); break;
+                    case 2: startneighbors[i].setNeighborsNodeDirection(neighborDirection.LEFT); break;
+                    case 3: startneighbors[i].setNeighborsNodeDirection(neighborDirection.RIGHT); break;
                 }
             }
         }
-
         
-        Node directionNode = null; //We dont know which direction to go
-        while (!found) { //While we havent found pacman
-            if (possibleNodes.isEmpty()) { 
-                break;
-            }
-            //System.out.println("Possible nodes: " + possibleNodes);
-            Node workingnode = possibleNodes.get(0); //get the first node from the list
-            ArrayList<Node> validNeibours = validNeibours(possibleNodes.get(0), seenNodes); //get all its valid neighbors, and set them to seen and their direction from startnode
-                for (Node validNode : validNeibours) { //for each neighbor node, check if node contains pacman
-                    if (isPacman(workingnode, targetNode)){
+        // Process queue until empty or found
+        while (!queue.isEmpty() && !found) {
+            Node current = queue.remove(0);
+            
+            // Check all neighbors of current node
+            Node[] neighbors = current.getNeighbourgs();
+            for (int i = 0; i < neighbors.length; i++) {
+                Node neighbor = neighbors[i];
+                if (neighbor != null && !neighbor.isSeen()) {
+                    neighbor.setSeen(true);
+                    seenNodes.add(neighbor);
+                    
+                    // Inherit direction from current node
+                    neighbor.setNeighborsNodeDirection(current.getNeighborsNodeDirection());
+                    
+                    // Check if this is Pacman
+                    if (neighbor == targetNode) {
                         found = true;
-                        System.out.println("Found pacman");
-                        directionNode = workingnode; //set direction ghost should go
-                        break;
+                        return directionFromNeighborDirection(current.getNeighborsNodeDirection());
                     }
-                    else {
-                        possibleNodes.add(validNode); //If not pacman, add them to the back of the queue
-                    }
+                    
+                    // Add to queue for processing
+                    queue.add(neighbor);
                 }
-                possibleNodes.remove(0); //remove worked node
-            
-            
-        }
-
-        for (Node node : seenNodes) { //for each node we have seen, set its direction from startnode
-            node.setSeen(false);
-        }
-        
-        if (directionNode != null) { 
-            switch (directionNode.getNeighborsNodeDirection()) {
-                case UP:
-                    return direction.LEFT;
-                case DOWN:
-                    return direction.RIGHT;
-                case LEFT:
-                    return direction.UP;
-                case RIGHT:
-                    return direction.DOWN;
-                default:
-                    break;
             }
         }
         
+        // If we get here, no path exists (should never happen in a proper maze)
+        System.out.println("WARNING: No path found to Pacman!");
         return direction.NONE;
+    }
+
+    private direction directionFromNeighborDirection(neighborDirection nd) {
+        switch (nd) {
+            case UP: return direction.UP;
+            case DOWN: return direction.DOWN;
+            case LEFT: return direction.LEFT;
+            case RIGHT: return direction.RIGHT;
+            default: return direction.NONE;
+        }
     }
 
     public boolean isPacman(Node node, Node targetNode) {
@@ -104,25 +111,24 @@ public class BFS {
     }
 
     public ArrayList<Node> validNeibours(Node node, ArrayList<Node> seenNodes) {
+        System.out.println("checking valid neighbours");
         Node[] neighbours = node.getNeighbourgs();
-        ArrayList<Node> validNeibours = new ArrayList<>();
-        
-        //System.out.println("NEIGHBOURS: " + Arrays.toString(neighbours));
-
-        for (int i = 0; i < neighbours.length; i++) {
-            //System.out.println("Checking neighbour: " + neighbours[i]);
-            if (neighbours[i] != null) {
-                //System.out.println("Neighbour is: " + neighbours[i].isSeen());
-            }
-            if (neighbours[i] != null && !neighbours[i].isSeen()) {
-                neighbours[i].setSeen(true);
-                seenNodes.add(neighbours[i]);
-                //System.out.println("Setting neighbour: " + neighbours[i] + " to seen");
-                validNeibours.add(neighbours[i]);
-                //System.out.println("Adding neighbour: " + neighbours[i]);
-                validNeibours.get(i).setNeighborsNodeDirection(node.getNeighborsNodeDirection());
+        System.out.println("Neighbours: " + neighbours.length);
+        for (Node n : neighbours) {
+            if (n != null) {
+                System.out.println(n.isSeen());
             }
         }
+        ArrayList<Node> validNeibours = new ArrayList<>();
+        for (int i = 0; i < neighbours.length; i++) {
+            if (neighbours[i] != null && !neighbours[i].isSeen()) {
+                neighbours[i].setSeen(true);
+                neighbours[i].setNeighborsNodeDirection(node.getNeighborsNodeDirection());
+                seenNodes.add(neighbours[i]);
+                validNeibours.add(neighbours[i]);
+            }
+        }
+        System.out.println("Valid neighbours: " + validNeibours.size());
         return validNeibours;
     }
 }
